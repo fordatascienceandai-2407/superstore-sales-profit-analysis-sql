dab11d42d22b62a25a34df1011a9a4e21d4dd126
Akshaya kumar Gupta <fordatascienceandai@gmail.com>
2026-06-22 20:21:59 +0530

    Update 01_top_customers.sql

--- sql_queries/01_top_customers.sql
+++ sql_queries/01_top_customers.sql
@@ -1,2 +1,10 @@
 -- Business Question 1
 -- Top 5 Customers by Sales
+
+SELECT
+    customer_name,
+    SUM(sales) AS total_sales
+FROM orders
+GROUP BY customer_name
+ORDER BY total_sales DESC
+LIMIT 5;
